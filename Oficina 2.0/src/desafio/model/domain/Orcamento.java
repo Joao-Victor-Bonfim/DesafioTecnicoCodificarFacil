42d1a62782477bbabf4474b9bac39c7ee676d7a4
João Victor Bonfim Tavares Miguel <katerana2@gmail.com>
2019-05-16 03:55:31 -0300

    Classes de domínio  A partir do banco de dados as classes de domínio foram geradas automaticamente. Algumas alterações foram feitas aos documentos gerados para facilitar na implementação de regras de negócio.

---
+++ Oficina 2.0/src/desafio/model/domain/Orcamento.java
@@ -0,0 +1,170 @@
+package desafio.model.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author João Victor Bonfim
+ */
+@Entity
+@Table(name = "orcamento")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Orcamento.findAll", query = "SELECT o FROM Orcamento o"),
+    @NamedQuery(name = "Orcamento.findByIdOrcamento", query = "SELECT o FROM Orcamento o WHERE o.idOrcamento = :idOrcamento"),
+    @NamedQuery(name = "Orcamento.findByDataEHora", query = "SELECT o FROM Orcamento o WHERE o.dataEHora = :dataEHora"),
+    @NamedQuery(name = "Orcamento.findByDescricao", query = "SELECT o FROM Orcamento o WHERE o.descricao = :descricao"),
+    @NamedQuery(name = "Orcamento.findByValor", query = "SELECT o FROM Orcamento o WHERE o.valor = :valor")})
+public class Orcamento implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_orcamento")
+    private Integer idOrcamento;
+    @Basic(optional = false)
+    @Column(name = "data_e_hora")
+    @Temporal(TemporalType.DATE)
+    private Date dataEHora;
+    @Basic(optional = false)
+    @Column(name = "descricao")
+    private String descricao;
+    @Basic(optional = false)
+    @Column(name = "valor")
+    private double valor;
+    @JoinColumn(name = "cliente", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Cliente cliente;
+    @JoinColumn(name = "vendedor", referencedColumnName = "nro_registro")
+    @ManyToOne(optional = false)
+    private Vendedor vendedor;
+
+    public Orcamento() {
+    }
+
+    public Orcamento(Integer idOrcamento) {
+        this.idOrcamento = idOrcamento;
+    }
+
+    public Orcamento(Integer idOrcamento, Date dataEHora, String descricao, double valor) {
+        this.idOrcamento = idOrcamento;
+        this.dataEHora = dataEHora;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public Integer getIdOrcamento() {
+        return idOrcamento;
+    }
+
+    public void setIdOrcamento(Integer idOrcamento) {
+        this.idOrcamento = idOrcamento;
+    }
+
+    public Date getDataEHora() {
+        return dataEHora;
+    }
+
+    public void setDataEHora(Date dataEHora) {
+        this.dataEHora = dataEHora;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+        this.cliente.getOrcamentoList().add(this);
+    }
+
+    public Vendedor getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(Vendedor vendedor) {
+        this.vendedor = vendedor;
+        this.vendedor.getOrcamentoList().add(this);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idOrcamento != null ? idOrcamento.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        
+        if (!(object instanceof Orcamento)) {
+            return false;
+        }
+        
+        Orcamento other = (Orcamento) object;
+        return !((this.idOrcamento == null && other.idOrcamento != null) || (this.idOrcamento != null && !this.idOrcamento.equals(other.idOrcamento)));
+    }
+
+    @Override
+    public String toString() {
+        return "desafio.model.domain.Orcamento[ idOrcamento=" + idOrcamento + " ]";
+    }
+    
+    public static boolean isValidValor(Double valor) {
+        //TO DO
+        return valor != null;
+    }
+    
+    public static boolean isValidIdOrcamento(Integer idOrcamento) {
+        if(idOrcamento == null)
+            return false;
+        return idOrcamento >= 0;
+    }
+    
+    public static boolean isValidDescricao(String descricao) {
+        if (descricao == null)
+            return false;
+        return !descricao.isEmpty();
+    }
+    
+    public static boolean isValidDataEHora(Date dataEHora) {
+        if (dataEHora == null)
+            return false;
+        
+        Date agora = new Date();
+        return dataEHora.before(agora) || dataEHora.equals(agora);
+    }
+    
+}
---
+++ Oficina 2.0/src/desafio/model/domain/Vendedor.java
@@ -0,0 +1,112 @@
+package desafio.model.domain;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author João Victor Bonfim
+ */
+@Entity
+@Table(name = "vendedor")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Vendedor.findAll", query = "SELECT v FROM Vendedor v"),
+    @NamedQuery(name = "Vendedor.findByNroRegistro", query = "SELECT v FROM Vendedor v WHERE v.nroRegistro = :nroRegistro"),
+    @NamedQuery(name = "Vendedor.findByNome", query = "SELECT v FROM Vendedor v WHERE v.nome = :nome")})
+public class Vendedor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "nro_registro")
+    private Integer nroRegistro;
+    @Basic(optional = false)
+    @Column(name = "nome")
+    private String nome;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "vendedor")
+    private List<Orcamento> orcamentoList;
+
+    public Vendedor() {
+    }
+
+    public Vendedor(Integer nroRegistro) {
+        this.nroRegistro = nroRegistro;
+    }
+
+    public Vendedor(Integer nroRegistro, String nome) {
+        this.nroRegistro = nroRegistro;
+        this.nome = nome;
+    }
+
+    public Integer getNroRegistro() {
+        return nroRegistro;
+    }
+
+    public void setNroRegistro(Integer nroRegistro) {
+        this.nroRegistro = nroRegistro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @XmlTransient
+    public List<Orcamento> getOrcamentoList() {
+        return orcamentoList;
+    }
+
+    public void setOrcamentoList(List<Orcamento> orcamentoList) {
+        this.orcamentoList = orcamentoList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (nroRegistro != null ? nroRegistro.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Vendedor)) {
+            return false;
+        }
+        Vendedor other = (Vendedor) object;
+        return !((this.nroRegistro == null && other.nroRegistro != null) || (this.nroRegistro != null && !this.nroRegistro.equals(other.nroRegistro)));
+    }
+
+    @Override
+    public String toString() {
+        return "desafio.model.domain.Vendedor[ nroRegistro=" + nroRegistro + " ]";
+    }
+    
+    public static boolean isValidNome(String nome) {
+        //TO DO
+        return true;
+    }
+    
+    public static boolean isValidNroRegistro(Integer nroRegistro) {
+        return nroRegistro >= 0;
+    }
+    
+    public static boolean isValidVendedor(Vendedor vendedor) {
+        return isValidNome(vendedor.nome) && isValidNroRegistro(vendedor.nroRegistro);
+    }
+    
+}
